e2d581e4e01be7592bd9b1bff8071cfd124c1024
Leonid Gavrilov <113637497+lxstcxntxry@users.noreply.github.com>
2022-11-05 00:36:12 +0300

    Update lab3_3.py
    with supporting English letters

--- inf 1 sem/lab3_3.py
+++ inf 1 sem/lab3_3.py
@@ -1,42 +1,34 @@
 import re
 
-isu = 331484 % 5
-# мой вариант - 4
-
-test1 = "ааУаааУаааУаа"  # TRUE
-test2 = "УуааУаааУ"  # SELECTED WORD IN INTERVAL
-test3 = "УааУааааааУ"  # FALSE BY DISTANCE
-test4 = "УааУааУааУааУ"  # FALSE BY SELECT
+test1 = "ааУаааУаааУаа"
+test2 = "ИfffИfffИ"
+test3 = "УааУааааааУ"
+test4 = "УааУааУааУааУ"
 test5 = "ааУаауУааааауУаа"
 tests = [test1, test2, test3, test4, test5]
 
 for test in tests:
-
     distance = 0
     flag = False
-
-    intervals = re.split(r"[A-Я]", test)
-    select = re.findall(r"[A-Я]", test)
+    intervals = re.split(r"[A-ZА-Я]", test)
+    select = re.findall(r"[A-ZА-Я]", test)
 
     if len(select) == 3:
         interval = [intervals[1], intervals[2]]
-
         for j in interval:
             if j:
                 if (select[0].lower() in j) or (select[1].lower() in j) or (select[2].lower() in j):
-                    print("Test", tests.index(test) + 1, "SELECTED WORD IN INTERVAL")
+                    print("Test", tests.index(test) + 1, "ошибка: Буквы в интервале")
                     break
                 else:
                     if distance == len(j):
-                        print("Test", tests.index(test) + 1, "TRUE")
+                        print("Test", tests.index(test) + 1, "+")
                         break
                     else:
                         distance = len(j)
-
                     if flag:
-                        print("Test", tests.index(test) + 1, "FALSE BY DISTANCE")
+                        print("Test", tests.index(test) + 1, "ошибка: Длина промежутков !=")
                         break
                     flag = True
-
     else:
-        print("Test", tests.index(test) + 1, "FALSE BY SELECT")
+        print("Test", tests.index(test) + 1, "ошибка: Выбрано много")
