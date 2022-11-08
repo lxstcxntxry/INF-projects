d5de5959c72faa5757888721614104dd064b5ef7
Leonid Gavrilov <113637497+lxstcxntxry@users.noreply.github.com>
2022-11-08 19:44:48 +0300

    Create lab4_libs_worktime.py

---
+++ 1 sem 4 lab/lab4_libs_worktime.py
@@ -0,0 +1,31 @@
+import json
+from datetime import datetime
+
+
+def reWriting(json_obj, line_padding=""):
+    result = list()
+    json_obj_type = type(json_obj)
+
+    if json_obj_type is dict:
+        for tag in json_obj:
+            sub_obj = json_obj[tag]
+            result.append(line_padding + "<" + tag + ">")
+            result.append(reWriting(sub_obj, "\t" + line_padding))
+            result.append(line_padding + "</" + tag + ">")
+
+        return "\n".join(result)
+
+    return line_padding + json_obj
+
+
+start_time = datetime.now()
+
+for i in range(100):
+    json_file = open("test.json", "r", encoding="utf-8")
+    xml_file = open("test_libs.xml", "w", encoding="utf-8")
+    xml_file.write(reWriting(json.load(json_file)))
+
+    json_file.close()
+    xml_file.close()
+
+print(datetime.now() - start_time)
