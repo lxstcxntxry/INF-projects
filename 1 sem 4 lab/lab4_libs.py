7aa4c2e664f9ef64702c82fb82480446a0012d7a
Leonid Gavrilov <113637497+lxstcxntxry@users.noreply.github.com>
2022-11-08 18:48:30 +0300

    Update lab4_libs.py

--- 1 sem 4 lab/lab4_libs.py
+++ 1 sem 4 lab/lab4_libs.py
@@ -6,18 +6,18 @@ def reWriting(json_obj, line_padding=""):
     json_obj_type = type(json_obj)
 
     if json_obj_type is dict:
-        for tag_name in json_obj:
-            sub_obj = json_obj[tag_name]
-            result.append(line_padding + "<" + tag_name + ">")
+        for tag in json_obj:
+            sub_obj = json_obj[tag]
+            result.append(line_padding + "<" + tag + ">")
             result.append(reWriting(sub_obj, "\t" + line_padding))
-            result.append(line_padding + "</" + tag_name + ">")
+            result.append(line_padding + "</" + tag + ">")
 
         return "\n".join(result)
 
     return line_padding + json_obj
 
 
-json_file = open("test_libs.json", "r", encoding="utf-8")
+json_file = open("test.json", "r", encoding="utf-8")
 xml_file = open("test_libs.xml", "w", encoding="utf-8")
 xml_file.write(reWriting(json.load(json_file)))
 
